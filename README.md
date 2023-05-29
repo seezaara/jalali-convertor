13bc9d434b3fb09a62546e8861f50f200592cd28
seezaara <123152498+seezaara@users.noreply.github.com>
2023-05-29 14:06:40 +0330

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# jalali-convertor
+# Jalaali JavaScript
+
 fastest javascript functions for converting Jalaali (Jalali, Persian, Khayyami, Khorshidi, Shamsi) and Gregorian calendar systems to each other.
+
+## Usage
+
+```js
+const date = require("jalali-convertor")
+
+// get curent date
+console.log(date.jDate());  // [ 1402, 3, 4 ]
+console.log(date.gDate());  // [ 2023, 5, 25 ]
+
+// get curent timestamp
+console.log(date.jTime());  // 1685018002138
+console.log(date.gTime());  // 1685018002138
+console.log(new Date(date.jTime()));  // 2023-05-25T12:41:31.417Z
+console.log(new Date(date.gTime()));  // 2023-05-25T12:41:31.418Z
+
+// get specific date
+console.log(date.jDate(1685014909046));  // [ 1402, 3, 4 ]
+console.log(date.gDate(1685014909046));  // [ 2023, 5, 25 ]
+
+// get specific timestamp
+console.log(date.jTime(1402, 3, 4));  // 1687638600000
+console.log(date.gTime(2023, 5, 25));  // 1687638600000
+console.log(new Date(date.jTime(1402, 3, 4)));  // 2023-05-24T20:30:00.000Z
+console.log(new Date(date.gTime(2023, 5, 25)));  // 2023-05-24T20:30:00.000Z
+
+// convert date
+console.log(date.j2g(1402, 3, 4));  // [ 2023, 5, 25 ]
+console.log(date.g2j(2023, 5, 25));  // [ 1402, 3, 4 ] 
+
+```
+ 
+
+
+
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,78 @@
+function jDate(...date) {
+    date = new Date(...date)
+    return g2j(date.getFullYear(), date.getMonth() + 1, date.getDate())
+}
+
+function gDate(...date) {
+    date = new Date(...date)
+    return [date.getFullYear(), date.getMonth() + 1, date.getDate()]
+}
+
+function jTime(jy, jm, jd, ...date) {
+    if (!jy)
+        return new Date().getTime()
+    const jl = j2g(jy, jm, jd)
+    if (jl[1])
+        jl[1]--
+    return new Date(...jl, ...date).getTime()
+}
+
+function gTime(...date) {
+    if (date[1])
+        date[1]--
+    return new Date(...date).getTime()
+}
+
+function g2j(gy, gm, gd) {
+    var g_d_m = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334];
+    var jy = (gy <= 1600) ? 0 : 979;
+    gy -= (gy <= 1600) ? 621 : 1600;
+    var gy2 = (gm > 2) ? (gy + 1) : gy;
+    var days = (365 * gy) + (parseInt((gy2 + 3) / 4)) - (parseInt((gy2 + 99) / 100))
+        + (parseInt((gy2 + 399) / 400)) - 80 + gd + g_d_m[gm - 1];
+    jy += 33 * (parseInt(days / 12053));
+    days %= 12053;
+    jy += 4 * (parseInt(days / 1461));
+    days %= 1461;
+    jy += parseInt((days - 1) / 365);
+    if (days > 365) days = (days - 1) % 365;
+    var jm = (days < 186) ? 1 + parseInt(days / 31) : 7 + parseInt((days - 186) / 30);
+    var jd = 1 + ((days < 186) ? (days % 31) : ((days - 186) % 30));
+    return [jy, jm, jd];
+}
+
+function j2g(jy, jm, jd) {
+    var gy = (jy <= 979) ? 621 : 1600;
+    jy -= (jy <= 979) ? 0 : 979;
+    var days = (365 * jy) + ((parseInt(jy / 33)) * 8) + (parseInt(((jy % 33) + 3) / 4))
+        + 78 + jd + ((jm < 7) ? (jm - 1) * 31 : ((jm - 7) * 30) + 186);
+    gy += 400 * (parseInt(days / 146097));
+    days %= 146097;
+    if (days > 36524) {
+        gy += 100 * (parseInt(--days / 36524));
+        days %= 36524;
+        if (days >= 365) days++;
+    }
+    gy += 4 * (parseInt((days) / 1461));
+    days %= 1461;
+    gy += parseInt((days - 1) / 365);
+    if (days > 365) days = (days - 1) % 365;
+    var gd = days + 1;
+    var sal_a = [0, 31, ((gy % 4 == 0 && gy % 100 != 0) || (gy % 400 == 0)) ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    var gm
+    for (gm = 0; gm < 13; gm++) {
+        var v = sal_a[gm];
+        if (gd <= v) break;
+        gd -= v;
+    }
+    return [gy, gm, gd];
+}
+
+module.exports = {
+    j2g,
+    g2j,
+    jDate,
+    gDate,
+    jTime,
+    gTime,
+}
